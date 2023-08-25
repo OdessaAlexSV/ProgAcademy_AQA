b3cf8db17314efa9b3883879311eb012a084356d
OdessaAlexSV <sav75.odessa@gmail.com>
2023-08-25 12:44:23 +0100

    added module Lesson4 with package classwork

---
+++ Lesson4_Streams/src/main/java/org/prog/classwork/car/Car.java
@@ -0,0 +1,11 @@
+package org.prog.classwork.car;
+
+public class Car {
+
+  public Owner owner;
+  public String plateNumber;
+
+  //TODO: HOME WORK
+  public String color;
+
+}
---
+++ Lesson4_Streams/src/main/java/org/prog/classwork/car/Owner.java
@@ -0,0 +1,8 @@
+package org.prog.classwork.car;
+
+public class Owner {
+
+  public String name;
+  public String lastName;
+
+}

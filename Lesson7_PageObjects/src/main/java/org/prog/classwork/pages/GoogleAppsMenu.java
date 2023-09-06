27c6ab6a6004447c1f48ecdf6489a0496971014b
OdessaAlexSV <sav75.odessa@gmail.com>
2023-09-06 22:28:21 +0100

    added classwork lesson7

---
+++ Lesson7_PageObjects/src/main/java/org/prog/classwork/pages/GoogleAppsMenu.java
@@ -0,0 +1,16 @@
+package org.prog.classwork.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class GoogleAppsMenu {
+
+  private final WebDriver driver;
+
+  public GoogleAppsMenu(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public void selectApp(String appName) {
+    //TODO: add code that selects google app
+  }
+}
